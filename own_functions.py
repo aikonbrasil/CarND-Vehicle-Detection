ed9c1a8191e68188680ad953ebca4945b9997d70
Dick Carrillo Melgarejo <dcarrillom@gmail.com>
2018-03-12 01:31:17 -0300

    updating script

--- own_functions.py
+++ own_functions.py
@@ -21,8 +21,8 @@ def extract_features(imgs, color_space='YCrCb', spatial_size=(16, 16),hist_bins=
     hist_range = hist_range
     imaage = cv2.imread(imgs[0])
     imaage = cv2.cvtColor(imaage, cv2.COLOR_BGR2RGB)
-    print(imaage.shape)
-    print(imgs[0])
+    #print(imaage.shape)
+    #print(imgs[0])
     for file in imgs:
         image = cv2.imread(file)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -41,46 +41,7 @@ def extract_features(imgs, color_space='YCrCb', spatial_size=(16, 16),hist_bins=
         hist_features = color_hist(feature_image, nbins=hist_bins, bins_range=hist_range)
         features.append(np.concatenate((spatial_features, hist_features, hog_features)))
     return features
-    #return imaage
 
-
-    # for file in imgs:
-    #     image = cv2.imread(file)
-    #     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    #     # apply color conversion if other than 'RGB'
-    #     if cspace != 'RGB':
-    #         if cspace == 'HSV':
-    #             feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    #         elif cspace == 'LUV':
-    #             feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
-    #         elif cspace == 'HLS':
-    #             feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
-    #         elif cspace == 'YUV':
-    #             feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
-    #         elif cspace == 'YCrCb':
-    #             feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
-    #     else: feature_image = np.copy(image)
-    #
-	# 	# Call get_hog_features() with vis=False, feature_vec=True
-	# 	if hog_channel == 'ALL':
-	# 		hog_features = []
-	# 		for channel in range(feature_image.shape[2]):
-	# 			hog_features.append(get_hog_features(feature_image[:,:,channel],
-	# 								orient, pix_per_cell, cell_per_block,
-	# 								vis=False, feature_vec=True))
-	# 		hog_features = np.ravel(hog_features)
-	# 	else:
-	# 		hog_features = get_hog_features(feature_image[:,:,hog_channel], orient,
-	# 					pix_per_cell, cell_per_block, vis=False, feature_vec=True)
-    #
-	# 	# Apply bin_spatial() to get spatial color features
-	# 	spatial_features = bin_spatial(feature_image, size)
-    #
-	# 	# Apply color_hist()
-	# 	hist_features = color_hist(feature_image, nbins=hist_bins, bins_range=hist_range)
-    #
-	# 	features.append(np.concatenate((spatial_features, hist_features, hog_features)))
-    # return features
 
 # Basic functions provided on Udacity's course to extract features.
 
@@ -192,3 +153,59 @@ def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
 
     #9) Return concatenated array of features
     return np.concatenate(img_features)
+
+# Here is your draw_boxes function from the previous exercise
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+    # Make a copy of the image
+    imcopy = np.copy(img)
+    # Iterate through the bounding boxes
+    for bbox in bboxes:
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
+    # Return the image copy with boxes drawn
+    return imcopy
+
+
+# Define a function that takes an image,
+# start and stop positions in both x and y,
+# window size (x and y dimensions),
+# and overlap fraction (for both x and y)
+def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None],
+                    xy_window=(64, 64), xy_overlap=(0.5, 0.5)):
+    # If x and/or y start/stop positions not defined, set to image size
+    if x_start_stop[0] == None:
+        x_start_stop[0] = 0
+    if x_start_stop[1] == None:
+        x_start_stop[1] = img.shape[1]
+    if y_start_stop[0] == None:
+        y_start_stop[0] = 0
+    if y_start_stop[1] == None:
+        y_start_stop[1] = img.shape[0]
+    # Compute the span of the region to be searched
+    xspan = x_start_stop[1] - x_start_stop[0]
+    yspan = y_start_stop[1] - y_start_stop[0]
+    # Compute the number of pixels per step in x/y
+    nx_pix_per_step = np.int(xy_window[0]*(1 - xy_overlap[0]))
+    ny_pix_per_step = np.int(xy_window[1]*(1 - xy_overlap[1]))
+    # Compute the number of windows in x/y
+    nx_buffer = np.int(xy_window[0]*(xy_overlap[0]))
+    ny_buffer = np.int(xy_window[1]*(xy_overlap[1]))
+    nx_windows = np.int((xspan-nx_buffer)/nx_pix_per_step)
+    ny_windows = np.int((yspan-ny_buffer)/ny_pix_per_step)
+    # Initialize a list to append window positions to
+    window_list = []
+    # Loop through finding x and y window positions
+    # Note: you could vectorize this step, but in practice
+    # you'll be considering windows one by one with your
+    # classifier, so looping makes sense
+    for ys in range(ny_windows):
+        for xs in range(nx_windows):
+            # Calculate window position
+            startx = xs*nx_pix_per_step + x_start_stop[0]
+            endx = startx + xy_window[0]
+            starty = ys*ny_pix_per_step + y_start_stop[0]
+            endy = starty + xy_window[1]
+            # Append window position to list
+            window_list.append(((startx, starty), (endx, endy)))
+    # Return the list of windows
+    return window_list
